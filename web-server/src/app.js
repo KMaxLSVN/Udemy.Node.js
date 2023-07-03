6947410bfa9e37784f47ef17eb6a5b379a3f9ef3
Max Kim <maxim250291@gmail.com>
2023-07-03 20:59:01 +0300

    Section 8 - Accesing API from Browser

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/weatherstack');
 
 const app = express();
 
@@ -40,11 +42,43 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
-  res.send({
-    forecast: 'It is snowing',
-    location: 'Philadelphia',
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address!',
+    });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
+      });
+    });
   });
 });
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide search term',
+    });
+  }
+  console.log(req.query);
+  res.send({
+    products: [],
+  });
+});
+
 app.get('/help/*', (req, res) => {
   res.render('404', {
     title: '404',
